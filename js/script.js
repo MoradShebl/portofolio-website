2bf6152b7396b7b9bb6a180021a845e1c6191ab0
Morad Shebl <114817417+MoradShebl@users.noreply.github.com>
2024-07-08 16:29:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// window.onload = function() {
+//     window.scrollTo(0, 0);
+//   }
+  
+  document.getElementById('check').addEventListener('change', function() {
+    const navbar = document.querySelector('.navbar');
+    if (this.checked) {
+        navbar.style.left = '0';
+    } else {
+        navbar.style.left = '-200%';
+    }
+    const checkbtn = document.querySelector('.checkbtn');
+    if (this.checked) {
+        checkbtn.style.color = '#d0d0d0';
+    } else {
+        checkbtn.style.color = '#000000';
+    }
+    
+});
